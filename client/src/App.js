42113222de8f1e6fae520aa884994299ef84d106
irminaj <irminaj@github.com>
2023-07-26 22:01:51 +0300

    delete unnecessary files, create and add global styles and theme provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Theme } from "./Theme";
+import GlobalCSS from "./globalStyles/GlobalStyles.style";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Theme>
+        <GlobalCSS />
+        <h1>Hello</h1>
+      </Theme>
     </div>
   );
 }
