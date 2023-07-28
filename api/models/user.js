0e36f57e60270aaeaf7146efe051770014aeacee
irminaj <irminaj@github.com>
2023-07-29 02:01:34 +0300

    install mongoose and create database model for user

---
+++ api/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+// const userSchema = new mongoose.Schema({
+//   firstName: {
+//     type: String,
+//     required: true,
+//   },
+//   lastName: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//   },
+//   birthDate: {
+//     type: Date,
+//   },
+// });
+
+const UserModel = mongoose.model("users", userSchema);
+module.exports = UserModel;
