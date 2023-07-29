333260c36cb0d3063dd103b0401ac1df9b2583b3
irminaj <irminaj@github.com>
2023-07-29 12:33:16 +0300

    change get and post routers handling by using mongoose

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,23 +1,39 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
 const router = express.Router();
+const UserModel = require("../models/user.js");
 
 users = [];
 
+mongoose.connect("mongodb://localhost:27017/mongotest");
+
 router.get("/", async (req, res) => {
-  const client = await new MongoClient("mongodb://localhost:27017").connect();
-  const usersArray = await client.db("mongotest").collection("users").find({}).toArray();
-  await client.close();
-  res.send(usersArray);
+  const users = await UserModel.find({});
+  res.send(users);
 });
 
 router.post("/", async (req, res) => {
-  const newUser = req.body;
-  const client = await new MongoClient("mongodb://localhost:27017").connect();
-  await client.db("mongotest").collection("users").insertOne(newUser);
-  await client.close();
-  res.status(201).send(newUser);
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+  res.status(201).send(user);
 });
+
+// router.get("/", async (req, res) => {
+//   const client = await new MongoClient("mongodb://localhost:27017").connect();
+//   const usersArray = await client.db("mongotest").collection("users").find({}).toArray();
+//   await client.close();
+//   res.send(usersArray);
+// });
+
+// router.post("/", async (req, res) => {
+//   const newUser = req.body;
+//   const client = await mongoose.connect("mongodb://localhost:27017");
+//   await client.db("mongotest").collection("users").insertOne(newUser);
+//   await client.close();
+//   res.status(201).send(newUser);
+// });
 
 router.patch("/:id", async (req, res) => {
   const userId = req.params.id;
